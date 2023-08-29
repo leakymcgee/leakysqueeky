b63a146d662f4d085f35d9853a7ad6ad1e78d93b
leakymcgee <86233108+leakymcgee@users.noreply.github.com>
2023-08-29 15:22:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 apikey=1234
 aws_access_key_id = AKIAVD32IN6IC4F6LGXF
 aws_secret_access_key = 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
+
+
+[default]
+aws_access_key_id = AKIA2OGYBAH6ZJAFIHXI
+aws_secret_access_key = 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
+output = json
+region = us-east-2
